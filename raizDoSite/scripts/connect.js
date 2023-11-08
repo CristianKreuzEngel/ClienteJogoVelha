c0d919b33b3409f1110710ef1e6859aff1076a22
Cristian Kreuz Engel <kreuzengelc@gmail.com>
2023-11-07 23:53:55 -0300

    parte inicial da função post para entrar

---
+++ raizDoSite/scripts/connect.js
@@ -0,0 +1,12 @@
+// Função POST
+function fazPost(url, body){
+
+}
+
+// Captura o valor do campo input
+function logar () {
+    event.preventDefault()
+    let url = "";
+    let name = document.getElementById("name").value;
+    console.log('teste')
+}
