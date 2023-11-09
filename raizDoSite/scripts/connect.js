88abc7ebf1b207b4a6a6ac0b6fa2c8736abfc188
Cristian Kreuz Engel <kreuzengelc@gmail.com>
2023-11-08 22:55:30 -0300

    parte final da função post

--- raizDoSite/scripts/connect.js
+++ raizDoSite/scripts/connect.js
@@ -1,12 +1,25 @@
 // Função POST
 function fazPost(url, body){
+    let request = new XMLHttpRequest();
+    request.open("POST", url, true);
+    request.setRequestHeader("Content-type", "application/json");
+    request.send(JSON.stringify(body));
 
-}
+    request.onload = function(){
+
+    }
+
+    return request.responseText;
+};
 
 // Captura o valor do campo input
 function logar () {
-    event.preventDefault()
+    event.preventDefault();
     let url = "";
     let name = document.getElementById("name").value;
-    console.log('teste')
-}
+    console.log('teste');
+
+    body = {
+        "name": name
+    }
+};
